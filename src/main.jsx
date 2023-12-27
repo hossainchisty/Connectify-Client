42cafdf37209466c018c40752b442785602f5b06
hossainchisty <hossain.chisty11@gmail.com>
2023-12-27 20:32:40 +0600

    fix: directory name changes

--- src/main.jsx
+++ src/main.jsx
@@ -5,19 +5,19 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
 } from 'react-router-dom';
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import Setting from './components/Setting.jsx';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import Setting from './components/Setting/Setting.jsx';
 import ApiKey from './components/ApiKey.jsx';
-import Overview from './components/Overview.jsx';
+import Overview from './components/Overview/Overview.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
       <Route path='/account/user-settings' element={<Setting />} />
-      <Route path='/account/api-keys' element={<ApiKey/>} />
-      <Route path='/account/overview' element={<Overview/>} />
+      <Route path='/account/api-keys' element={<ApiKey />} />
+      <Route path='/account/overview' element={<Overview />} />
     </Route>
   )
 );
@@ -25,5 +25,5 @@ const router = createBrowserRouter(
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router}>
     <App />
-  </RouterProvider>,
-)
+  </RouterProvider>
+);
